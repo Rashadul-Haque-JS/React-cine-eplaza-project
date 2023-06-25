c6e062324c2bb2147f386f8cd548e9b2138943f7
Rashadul Haque <rashadulhaque@Rashaduls-MacBook-Air.local>
2023-06-25 17:28:52 +0200

    updated with apikey

--- src/common/api/movies.js
+++ src/common/api/movies.js
@@ -1,5 +1,25 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export default axios.create({
-    baseURL :'http://www.omdbapi.com/',
-})
+// Create a new Axios instance
+const api = axios.create({
+  baseURL: 'http://www.omdbapi.com/',
+});
+
+
+api.interceptors.response.use(
+  (response) => {
+    // Modify headers to enable CORS
+    response.headers['Access-Control-Allow-Origin'] = '*';
+    response.headers['Access-Control-Allow-Methods'] = 'GET,OPTIONS';
+    response.headers['Access-Control-Allow-Headers'] =
+      'Origin, X-Requested-With, Content-Type, Accept';
+
+    return response;
+  },
+  (error) => {
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
